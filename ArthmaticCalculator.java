d8d7510153c80a5cfd14869bd228b8d388d9c320
sushgowda23 <98318280+sushgowda23@users.noreply.github.com>
2022-01-24 16:47:43 +0530

    Add files via upload

---
+++ ArthmaticCalculator.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+ class ArthmaticCalculator {
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the two numbers");
+		int firstnum=sc.nextInt();
+		int secnum= sc.nextInt();
+		System.out.println("Enter the operator ");
+		char output=sc.next().charAt(0);
+		double Ans=0;
+
+		switch(output){
+		case '+': Ans=firstnum+secnum;
+		break;
+		case '-':Ans=firstnum-secnum;
+		break;
+		case '*': Ans=firstnum*secnum;
+		break;
+		case '/':Ans=firstnum/secnum;
+		break;
+		}
+		System.out.println(" answer is " +Ans);
+	}
+}
