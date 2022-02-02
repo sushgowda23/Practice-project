2ea3b4c6190a00e1f54486bacdc42328e425975c
sushgowda23 <98318280+sushgowda23@users.noreply.github.com>
2022-02-02 15:45:54 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,49 @@
+class Inheritance  
+{ 
+    public int gear; 
+    public int speed; 
+    public Inheritance(int gear, int speed) 
+    { 
+        this.gear = gear; 
+        this.speed = speed; 
+    } 
+    public void applyBrake(int decrement) 
+    { 
+        speed -= decrement; 
+    } 
+    public void speedUp(int increment) 
+    { 
+        speed += increment; 
+    }  
+    public String toString()  
+    { 
+        return("No of gears are " + gear + "\n" + "speed of bicycle is " + speed); 
+    }  
+} 
+class MountainBike extends Inheritance  
+{ 
+    public int seatHeight; 
+    public MountainBike(int gear,int speed,int startHeight) 
+    {  
+        super(gear, speed); 
+        seatHeight = startHeight; 
+    }  
+    public void setHeight(int newValue) 
+    { 
+        seatHeight = newValue; 
+    } 
+    @Override
+    public String toString() 
+    { 
+        return (super.toString()+ 
+                "\nseat height is "+seatHeight); 
+    } 
+}
+public class test 
+{ 
+    public static void main(String args[])  
+    { 
+        MountainBike mb = new MountainBike(3, 100, 25); 
+        System.out.println(mb.toString());
+    } 
+}
